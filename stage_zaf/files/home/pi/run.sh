b26be9ab7735e98377d890e62ac6462e34641bdc
Sven Gothel <sgothel@jausoft.com>
2021-06-24 20:15:41 +0200

    /home/pi/: .xsession run.sh

---
+++ stage_zaf/files/home/pi/run.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+echo "\n"
+# sudo /etc/rc.local &
+# Change work directory
+cd /home/pi
+# load background
+xloadimage -onroot /home/pi/splash.png &
+# setup sound
+sudo amixer sset Master 100% 
+#sudo modprobe snd_pcm_oss
+sudo amixer sset PCM 100%
+# disable pointer
+unclutter -idle 5 -jitter 1 -root -noevents &
+# disable screensaver
+xset s 0 &
+xset dpms 0 0 0 &
+# Sync clock on startup
+sudo ntpdate-debian &
+# Launch Zafena main application
+cd /home/pi/run
+#sudo java --illegal-access=warn -Dsun.java2d.xrender=false zaf101openrdbarcodecommunicator.ZAF101BarcodeCommunicator
+glxgears
