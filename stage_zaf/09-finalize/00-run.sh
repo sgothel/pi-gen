f44c90b5d873fc45791e830b0b112f6bba0b6118
Sven Gothel <sgothel@jausoft.com>
2021-06-24 23:22:08 +0200

    stage_zaf: Fix location for 'overlay' files* folder

--- stage_zaf/09-finalize/00-run.sh
+++ stage_zaf/09-finalize/00-run.sh
@@ -3,9 +3,9 @@
 # disabled for now
 # tar -zxvf files/zafena.tar.gz -C "${ROOTFS_DIR}/"
 
-cp -av files/*            "${ROOTFS_DIR}/"
-cp -av files.elevator/*   "${ROOTFS_DIR}/"
-cp -av files.zafena_app/* "${ROOTFS_DIR}/"
+cp -av ../files/*            "${ROOTFS_DIR}/"
+cp -av ../files.elevator/*   "${ROOTFS_DIR}/"
+cp -av ../files.zafena_app/* "${ROOTFS_DIR}/"
 
 rm -fv /etc/systemd/system/dbus-org.bluez.service
 rm -fv /etc/systemd/system/bluetooth.target.wants/bluetooth.service
