718726130a79e61acdc6a339aea8342b566b289d
Sven Gothel <sgothel@jausoft.com>
2021-06-25 04:46:41 +0200

    stage_zaf: Disable rainbow splash, enable Zafena plymouth splash (sadly shown only for ~1s)

--- stage_zaf/09-finalize/00-run.sh
+++ stage_zaf/09-finalize/00-run.sh
@@ -7,6 +7,8 @@ cp -av ../files/*            "${ROOTFS_DIR}/"
 cp -av ../files.elevator/*   "${ROOTFS_DIR}/"
 cp -av ../files.zafena_app/* "${ROOTFS_DIR}/"
 
+cp -av ../files/home/pi/splash.png "${ROOTFS_DIR}/usr/share/plymouth/themes/pix/splash.png"
+
 cat ../files/home/pi/.bashrc_startx >> "${ROOTFS_DIR}/home/pi/.bashrc"
 
 rm -fv /etc/systemd/system/dbus-org.bluez.service
