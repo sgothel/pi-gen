6b3c29c6b8d01a8dbc6ba51e77bc1feb4cfe2e18
Sven Gothel <sgothel@jausoft.com>
2021-06-25 02:27:29 +0200

    stage_zaf: 'systemctl mask bluetooth' via symlink /dev/null -> /etc/systemd/system/bluetooth.service

--- stage_zaf/09-finalize/00-run.sh
+++ stage_zaf/09-finalize/00-run.sh
@@ -9,4 +9,6 @@ cp -av ../files.zafena_app/* "${ROOTFS_DIR}/"
 
 rm -fv /etc/systemd/system/dbus-org.bluez.service
 rm -fv /etc/systemd/system/bluetooth.target.wants/bluetooth.service
+# systemctl mask bluetooth
+ln -s /dev/null /etc/systemd/system/bluetooth.service
 
