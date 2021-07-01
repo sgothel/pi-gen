447dd404b4d78e3821bd16e66c00c1d5dff81ebc
Sven Gothel <sgothel@jausoft.com>
2021-07-01 06:39:41 +0200

    disable swap

--- stage1/01-sys-tweaks/00-run.sh
+++ stage1/01-sys-tweaks/00-run.sh
@@ -5,12 +5,12 @@ install -m 644 files/noclear.conf "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.s
 install -v -m 644 files/fstab "${ROOTFS_DIR}/etc/fstab"
 
 on_chroot << EOF
-# Memory ~1G -> Swap 2G; BUT slow and small sd-card 
-# Use 200MB (Raspberry comes with 100MB default)
-rm -f /data/swapfile
-dd if=/dev/zero of=/data/swapfile bs=1M count=200
-mkswap /data/swapfile
-chmod 600 /data/swapfile
+## Memory ~1G -> Swap 2G; BUT slow and small sd-card 
+## Use 200MB (Raspberry comes with 100MB default)
+#rm -f /data/swapfile
+#dd if=/dev/zero of=/data/swapfile bs=1M count=200
+#mkswap /data/swapfile
+#chmod 600 /data/swapfile
 
 if ! id -u ${FIRST_USER_NAME} >/dev/null 2>&1; then
 	adduser --disabled-password --gecos "" ${FIRST_USER_NAME}
