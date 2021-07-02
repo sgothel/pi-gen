b4e072351b82eb654c90daa8454b62f00b367f91
Sven Gothel <sgothel@jausoft.com>
2021-07-02 06:37:55 +0200

    install_deps: add squashfs-tools

--- scripts-zaf/00-install-deps.sh
+++ scripts-zaf/00-install-deps.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-apt-get install coreutils quilt parted qemu-user-static debootstrap zerofree zip dosfstools libarchive-tools libcap2-bin grep rsync xz-utils file git curl bc qemu-utils kpartx
+apt-get install coreutils quilt parted qemu-user-static debootstrap zerofree zip dosfstools libarchive-tools libcap2-bin grep rsync xz-utils file git curl bc qemu-utils kpartx squashfs-tools
