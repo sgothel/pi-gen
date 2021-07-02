821c0f9620c4ec2431a913c2893b17e966374af6
Sven Gothel <sgothel@jausoft.com>
2021-07-02 06:37:24 +0200

    stage2: Add mkinitramfs w/ forced fsck types; journald.conf memory only etc settings; tmpfs overlay_mount (for now); loop_rootfs: single /boot fsck ...;
    Use overlay_mount on a tmpfs 64MiB (for now)
    
    loop_rootfs: single /boot fsck and add debug panic at end to validated results
    
    /etc/systemd/journald.conf
    - Storage=volatile
    - Compress=yes
    - RuntimeMaxUse=1M
    - ForwardToSyslog=no
    - ForwardToWall=no

--- stage2/01-sys-tweaks/01-run.sh
+++ stage2/01-sys-tweaks/01-run.sh
@@ -27,43 +27,55 @@ s/^#?[[:blank:]]*PasswordAuthentication[[:blank:]]*yes[[:blank:]]*$/PasswordAuth
 fi
 
 on_chroot << EOF
-systemctl disable hwclock.sh
-#systemctl disable nfs-common
-systemctl disable rpcbind
-if [ "${ENABLE_SSH}" == "1" ]; then
-	systemctl enable ssh
-else
-	systemctl disable ssh
-fi
-systemctl enable regenerate_ssh_host_keys
+    systemctl disable hwclock.sh
+    #systemctl disable nfs-common
+    systemctl disable rpcbind
+    if [ "${ENABLE_SSH}" == "1" ]; then
+        systemctl enable ssh
+    else
+        systemctl disable ssh
+    fi
+    systemctl enable regenerate_ssh_host_keys
 
-#mkdir -p /data/overlay_a
-#cd /data/overlay_a
-#mkdir -p etc/upper etc/work home/upper home/work srv/upper srv/work tmp/upper tmp/work var/upper var/work
-#
-#mkdir -p /data/overlay_b
-#cd /data/overlay_b
-#mkdir -p etc/upper etc/work home/upper home/work srv/upper srv/work tmp/upper tmp/work var/upper var/work
-#
-#cd /data
-#ln -s overlay_a overlay
-#
-#systemctl enable overlay_mount
+    #mkdir -p /data/overlay_a
+    #cd /data/overlay_a
+    #mkdir -p etc/upper etc/work home/upper home/work srv/upper srv/work tmp/upper tmp/work var/upper var/work
+    #
+    #mkdir -p /data/overlay_b
+    #cd /data/overlay_b
+    #mkdir -p etc/upper etc/work home/upper home/work srv/upper srv/work tmp/upper tmp/work var/upper var/work
+    #
+    #cd /data
+    #ln -s overlay_a overlay
+    #
 
-systemctl disable overlay_mount
-systemctl mask overlay_mount
+    systemctl unmask overlay_mount
+    systemctl enable overlay_mount
 
-# Setup unique system folder /boot/sys_arm64_000
-mkdir -p /boot/sys_arm64_000
-mv /boot/*.dtb /boot/COPYING.linux /boot/LICENCE.broadcom /boot/initrd.img /boot/sys_arm64_000/
-mv /boot/issue.txt /boot/kernel8.img /boot/overlays /boot/rootfs.img /boot/sys_arm64_000/
+    #systemctl disable overlay_mount
+    #systemctl mask overlay_mount
 
-mv /boot/config.txt /boot/config.txt.orig
-mv /boot/cmdline.txt /boot/sys_arm64_000/cmdline.txt.orig
-if [ -f /boot/initrd.img ]; then
-    mv /boot/initrd.img /boot/sys_arm64_000/initrd.img.orig
-fi
+    # Setup unique system folder /boot/sys_arm64_000
+    mkdir -p /boot/sys_arm64_000
 
+    find /boot -maxdepth 1 -name \*.dtb -exec mv \{\} /boot/sys_arm64_000/ \;
+
+    find /boot -maxdepth 1 -name kernel\*.img -exec mv \{\} /boot/sys_arm64_000/ \;
+
+    for i in /boot/COPYING.linux /boot/LICENCE.broadcom /boot/sys_arm64_000/ \
+             /boot/issue.txt /boot/overlays /boot/rootfs.img ; do
+        if [ -f "${i}" ]; then
+             mv "${i}" /boot/sys_arm64_000/
+        fi
+    done
+
+    for i in /boot/config.txt /boot/cmdline.txt /boot/initrd.img ; do
+        if [ -f "${i}" ]; then
+             mv "${i}" /boot/sys_arm64_000/"${i}.orig"
+        fi
+    done
+
+    systemctl disable resize2fs_once
 EOF
 
 install -m 644 files/boot/sys_arm64_000/cmdline.txt 	"${ROOTFS_DIR}/boot/sys_arm64_000/"
@@ -71,46 +83,35 @@ install -m 644 files/boot/sys_arm64_000/cmdline.txt 	"${ROOTFS_DIR}/boot/sys_arm
 install -m 644 files/boot/config.txt 	"${ROOTFS_DIR}/boot/"
 
 install -m 755 files/initramfs/loop_rootfs 	"${ROOTFS_DIR}/etc/initramfs-tools/scripts/init-premount/"
+install -m 755 files/initramfs/fsck_custom 	"${ROOTFS_DIR}/etc/initramfs-tools/hooks/"
 
 # echo "squashfs" >> "${ROOTFS_DIR}/etc/modules"
 echo "squashfs" >> "${ROOTFS_DIR}/etc/initramfs-tools/modules"
 
-sed -i -e 's/MODULES=most/MODULES=dep/g' -e 's/BUSYBOX=auto/BUSYBOX=y/g' "${ROOTFS_DIR}/etc/initramfs-tools/initramfs.conf"
+sed -i -e 's/MODULES=most/MODULES=dep/g;s/BUSYBOX=auto/BUSYBOX=y/g' "${ROOTFS_DIR}/etc/initramfs-tools/initramfs.conf"
 
-on_chroot << EOF
-mkinitramfs -o /boot/sys_arm64_000/initrd.img
-EOF
+sed -i -e 's/#Storage=auto/Storage=volatile/g;s/#Compress=yes/Compress=yes/g;s/#RuntimeMaxUse=/RuntimeMaxUse=1M/g;s/#ForwardToSyslog=yes/ForwardToSyslog=no/g;s/#ForwardToWall=yes/ForwardToWall=no/g' "${ROOTFS_DIR}/etc/systemd/journald.conf"
 
 if [ "${USE_QEMU}" = "1" ]; then
 	echo "enter QEMU mode"
 	install -m 644 files/90-qemu.rules "${ROOTFS_DIR}/etc/udev/rules.d/"
-	on_chroot << EOF
-systemctl disable resize2fs_once
-EOF
 	echo "leaving QEMU mode"
-else
-	on_chroot << EOF
-#systemctl enable resize2fs_once
-systemctl disable resize2fs_once
-EOF
-	echo "leaving normal mode"
 fi
 
 on_chroot <<EOF
-for GRP in input spi i2c gpio; do
-	groupadd -f -r "\$GRP"
-done
-for GRP in adm dialout cdrom audio users sudo video games plugdev input gpio spi i2c netdev; do
-  adduser $FIRST_USER_NAME \$GRP
-done
-EOF
+    for GRP in input spi i2c gpio; do
+        groupadd -f -r "\$GRP"
+    done
+    for GRP in adm dialout cdrom audio users sudo video games plugdev input gpio spi i2c netdev; do
+      adduser $FIRST_USER_NAME \$GRP
+    done
 
-on_chroot << EOF
-setupcon --force --save-only -v
-EOF
+    setupcon --force --save-only -v
+    usermod --pass='*' root
 
-on_chroot << EOF
-usermod --pass='*' root
+    KVERSION=\$(ls /lib/modules/ | tail -n 1)
+    echo "mkinitramfs for kernel version: \${KVERSION}"
+    /usr/sbin/mkinitramfs -o /boot/sys_arm64_000/initrd.img \${KVERSION}
 EOF
 
 rm -f "${ROOTFS_DIR}/etc/ssh/"ssh_host_*_key*
