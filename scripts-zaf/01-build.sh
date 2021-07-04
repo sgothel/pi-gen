aa7f154882b4ec32cf5fff6ca94ac51809303361
Sven Gothel <sgothel@jausoft.com>
2021-07-04 05:24:27 +0200

    scripts-zaf: skip stage4

--- scripts-zaf/01-build.sh
+++ scripts-zaf/01-build.sh
@@ -40,10 +40,9 @@ export ENABLE_SSH=1
 export PUBKEY_SSH_FIRST_USER="ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAgEAm2qfo3uiWT9V7E/sH6unVO6XhGHeNnGUswxuADwFAKyflJq2yCALWIX7+DzEhBc7O9HxVztZAHLA+lfEpctQkA7KPgX98rjLHJlrm1lQXCPjwWjTZBrGNCmjWpYkc8jew/BzngmiwDlshYqzp0rWKNvQbkGJaVctqaraJAzE3F2wa+KkSBQuw2rAhiarbiO4MtO1AIw25EnSXHhP9EpeTKhAniKm73q977m5wpzaU+uwoGPCIyVl+BeaGLbPPIF35PJbg4tIqG6WLy3a8++Mf5Ml8lETcYYfjh4er3CvqFLhAYcaCLPNOwkK/YT4UnYZoZ4nt0Z+JWjxeNhIuJf64neysBdgB5E5FEYP2iYH+EUjcYp7YCrl/3/ag4Z2KAsF2qEa6/+s90Clm0r2mqRcQyLYQ6Co1YlYa1/jq7qsiuNgWvn5Mg8Pt4+L/C0KoycvUr4HhTS8EBJx+774p2uBa3mB65Vg2llKb8DdtQwR1bh0Ot9cjKTJY0pDyPiFbemZh2q7qD958bsOZCly43WYRd6E0Df68Q2MTz6ZC1y+z103gsEiK8bXmZp42ghDa96Ik2JO6pWCEUjBJ/9+rdudRHcfDlBqoS6+wg/z+WvtRtEyvVO4LSTrsX5xHWzVloSZeAGowmLbxGWHGWfl5oCcNfUzXo0lXDP0bzyy5NxWkpE= sven@phalanx"
 export PUBKEY_ONLY_SSH=1
 
-export STAGE_LIST="stage0 stage1 stage2 stage3 stage4 stage_zaf"
+export STAGE_LIST="stage0 stage1 stage2 stage3 stage_zaf"
 #export STAGE_LIST="stage0 stage1 stage2"
 
 touch ${rootdir}/stage2/SKIP_IMAGES
-touch ${rootdir}/stage4/SKIP_IMAGES
 
 ./build.sh
