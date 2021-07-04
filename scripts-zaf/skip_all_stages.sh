0d5cb0b31fb0c0700e81d11ca70c371bbb0a2e55
Sven Gothel <sgothel@jausoft.com>
2021-07-04 03:04:37 +0200

    Fix convenient scripts to skip/unskip all stages (missed stage2)

--- scripts-zaf/skip_all_stages.sh
+++ scripts-zaf/skip_all_stages.sh
@@ -5,6 +5,7 @@ rootdir=`dirname $sdir`
 
 touch ${rootdir}/stage0/SKIP
 touch ${rootdir}/stage1/SKIP
+touch ${rootdir}/stage2/SKIP
 touch ${rootdir}/stage3/SKIP
 touch ${rootdir}/stage4/SKIP
 touch ${rootdir}/stage5/SKIP
@@ -13,8 +14,8 @@ touch ${rootdir}/stage_zaf/SKIP
 touch ${rootdir}/stage2/SKIP_IMAGES
 touch ${rootdir}/stage4/SKIP_IMAGES
 
-echo SKIP
+echo SKIP Entries:
 find ${rootdir} -name SKIP
 
-echo SKIP_IMAGES
+echo SKIP_IMAGES Entries:
 find ${rootdir} -name SKIP_IMAGES
