e3d425fe3d9138ebfdb38db88a4bf0cfa1f5ebdf
Sven Gothel <sgothel@jausoft.com>
2021-07-18 20:09:34 +0200

    Update README.md; stage_rescue: Bring back visible menu w/ timeout=5s if ROOTFS_RO (allow memtest86+ selection)
    stage_rescue in ROOTFS_RO: Also remove default memtest menu-entries, we use our custom.cfg

--- stage_rescue/00-install-packages/01-run.sh
+++ stage_rescue/00-install-packages/01-run.sh
@@ -14,6 +14,8 @@ if [ "${ROOTFS_RO}" = "1" ] ; then
         sed -i "s/sys_amd64_000/sys_${TARGET_ARCH}_000/g"    "${ROOTFS_DIR}/boot/grub/custom.cfg"
         cp "${ROOTFS_DIR}/boot/grub/custom.cfg"              "${ROOTFS_DIR}/boot/sys_${TARGET_ARCH}_000/"
         sed -i 's/GRUB_TIMEOUT=.*$/GRUB_TIMEOUT=5/g'         "${ROOTFS_DIR}/etc/default/grub"
+        rm -f                                                "${ROOTFS_DIR}/etc/grub.d/20_memtest86"
+        rm -f                                                "${ROOTFS_DIR}/etc/grub.d/20_memtest86+"
     fi
 fi
 
@@ -21,6 +23,13 @@ on_chroot <<EOF
     if [ "${ROOTFS_RO}" = "1" ]; then
         mkdir -p /data/sdcard
         cp -a /boot/sys_${TARGET_ARCH}_000 /data/sdcard/
+    fi
+    if [ "${TARGET_RASPI}" != "1" ]; then
+        # Update grub.cfg for (1 ROOTFS_RO): timeout=5 or (2) normal memtest86+.bin menu entries
+        update-grub
+
+        # Remove storage device related 'search.fs_uuid' and allow multi homing
+        rm -f /boot/grub/i386-pc/load.cfg
     fi
 EOF
 
