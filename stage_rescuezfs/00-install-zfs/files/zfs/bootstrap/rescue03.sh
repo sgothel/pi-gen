40067aaf3b0681ac456da9a779e8c83245d2ce1b
Sven Gothel <sgothel@jausoft.com>
2021-07-22 13:30:44 +0200

    stage_rescuezfs: Simple ROOTFS_RO initrd.img updates, refresh /data/sdcard folder, add !ROOTFS_RO mode; ..
    Also fix rescue scripts: Executable and use rescue.cfg for configuration

--- stage_rescuezfs/00-install-zfs/files/zfs/bootstrap/rescue03.sh
+++ stage_rescuezfs/00-install-zfs/files/zfs/bootstrap/rescue03.sh
@@ -2,6 +2,6 @@
 
 export BASE_DIR=`dirname $(readlink -f "${BASH_SOURCE[0]}")`
 
-source "${BASE_DIR}/rescue00.sh"
+source "${BASE_DIR}/rescue.cfg"
 
 cat /proc/mounts | awk '{print $2}' | grep "${ROOTFS_DIR}" | sort -r | xargs umount
