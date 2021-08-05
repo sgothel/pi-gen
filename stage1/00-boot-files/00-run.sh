c21dc9ad068f8b1304d0903007a7ea67b95a04cb
Sven Gothel <sgothel@jausoft.com>
2021-08-05 19:52:58 +0200

    TARGET_RASPI and TARGET_ARCH=armhf: Disable 'arm_64bit=1' in config.txt files

--- stage1/00-boot-files/00-run.sh
+++ stage1/00-boot-files/00-run.sh
@@ -3,4 +3,7 @@
 if [ "${TARGET_RASPI}" = "1" ]; then
     install -m 644 files/cmdline.txt "${ROOTFS_DIR}/boot/"
     install -m 644 files/config.txt "${ROOTFS_DIR}/boot/"
+    if [ "${TARGET_ARCH}" = "armhf" ]; then
+        sed -i "s/arm_64bit=1/# arm_64bit=1/g"    "${ROOTFS_DIR}/boot/config.txt"
+    fi
 fi
