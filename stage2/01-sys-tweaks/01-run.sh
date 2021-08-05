14902a25bf09df61cae86dd7ffd0a6885da2ad36
Sven Gothel <sgothel@jausoft.com>
2021-08-05 21:04:04 +0200

    TARGET_RASPI and TARGET_ARCH=armhf: Disable 'arm_64bit=1' in config.txt files (also for mutable rootfs)

--- stage2/01-sys-tweaks/01-run.sh
+++ stage2/01-sys-tweaks/01-run.sh
@@ -210,6 +210,10 @@ else
         install -m 644 files/boot/sys_arm64_000/cmdline-rootfs_rw.txt  "${ROOTFS_DIR}/boot/sys_${TARGET_ARCH}_000/cmdline.txt"
         sed -i "s/sys_arm64_000/sys_${TARGET_ARCH}_000/g"    "${ROOTFS_DIR}/boot/config.txt"
         sed -i "s/sys_arm64_000/sys_${TARGET_ARCH}_000/g"    "${ROOTFS_DIR}/boot/sys_${TARGET_ARCH}_000/config.txt"
+        if [ "${TARGET_ARCH}" = "armhf" ]; then
+            sed -i "s/arm_64bit=1/# arm_64bit=1/g"               "${ROOTFS_DIR}/boot/config.txt"
+            sed -i "s/arm_64bit=1/# arm_64bit=1/g"               "${ROOTFS_DIR}/boot/sys_${TARGET_ARCH}_000/config.txt"
+        fi
     else
         sed -i 's/quiet//g'                                  "${ROOTFS_DIR}/etc/default/grub"
     fi
